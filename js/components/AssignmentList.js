c8e189633abd327a9f41ffd04d21aca3d8b0eb07
hafizflow <hafizur.rahman.cs@gmail.com>
2025-09-16 07:10:26 +0600

    Day 11

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,24 +1,26 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
+
 export default {
-    components: {Assignment},
+    components: {Assignment, AssignmentTags},
     template: `
         <section v-show="assignments.length">
             <h2 class="font-bold mb-2">{{ title }}
                 <span>({{ assignments.length }})</span>
             </h2>
 
-            <div class="flex gap-2">
-                <button 
-                    v-for="tag in tags" 
-                    class="px-1 py-px border rounded text-xs"
-                    :class="{'border-blue-500 text-blue-500': currentTag === tag}"
-                    @click="currentTag = tag"
-                    >{{ tag }}
-                </button>
-            </div>
+            <assignment-tags 
+                :initialTags="assignments.map(a => a.tag)"
+                :current-tag="currentTag"
+                @change="currentTag = $event"
+            />
     
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-                <assignment  v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
+                <assignment  
+                    v-for="assignment in filteredAssignments" 
+                    :key="assignment.id" 
+                    :assignment="assignment">
+                </assignment>
             </ul>
         </section>
     `,
@@ -40,10 +42,6 @@ export default {
                 return this.assignments;
             }
             return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))]
         }
     },
 }
---
+++ js/components/AssignmentTags.js
@@ -0,0 +1,25 @@
+export default {
+    template: `
+        <div class="flex gap-2">
+            <button 
+                v-for="tag in tags" 
+                class="px-1 py-px border rounded text-xs"
+                :class="{'border-blue-500 text-blue-500': currentTag === tag}"
+                @click="$emit('change', tag)"
+                >{{ tag }}
+            </button>
+        </div>
+    `,
+
+    props: {
+        initialTags: Array,
+        currentTag: String
+    },
+
+    computed: {
+        tags() {
+            return ['all', ...new Set(this.initialTags)];
+        }
+    }
+
+}
